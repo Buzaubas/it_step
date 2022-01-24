79fd1487054ddc7b36ae1bee6074a5f68e88136b
Dimash Bexultanov <94544861+buzaubas@users.noreply.github.com>
2022-01-24 21:37:37 +0600

    Create Sorting(New Array).cpp

---
+++ Sorting(New Array).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+int findMin(int* arr, int size) {
+    int min = arr[0];
+    int indexMin = 0;
+    for (int i = 0; i < size; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+            indexMin = i;
+        }
+    }
+
+    return indexMin;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    const int SIZE = 10;
+    int arr[SIZE];
+    int newArray[SIZE];
+    int minEl;
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 1000;
+    }
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < SIZE; i++) {
+        minEl = findMin(arr, SIZE);
+        newArray[i] = arr[minEl];
+        arr[minEl] = INT_MAX;
+    }
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << newArray[i] << " ";
+    }
+}
